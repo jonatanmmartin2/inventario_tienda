9effe01890d9fc476c4f98dd7c798acb133f021d
jonantanmmartin2 <aimarttin@gmail.com>
2023-10-04 18:00:24 -0500

    Primer commit, se realiza la estructura base del proyecto

---
+++ mian.py
@@ -0,0 +1,11 @@
+'''
+1. Agregar productos para vender. Nombre, percio y stock.
+2. Ver todo el inventario.
+3. Realizar una venta: Solicita nombre del producto y cantidad, si hay suficiente stock
+    se hace la venta y se da precio, tambien se descuenta el stock. En caso de no haber 
+    suficiente stock, mensaje que no se puede vender.
+4. Salir del programa.
+'''
+
+if __name__ == '__main__':
+    run()
